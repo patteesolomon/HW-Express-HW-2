a700a0d20bcf88b569cb9f97a98b82e4997960b3
patteesolomon <patteesolomon@gmail.com>
2022-12-01 20:27:46 -0600

    whfiw

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var magic8 = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+var hmax = 0;
+var hmin = 18;
+
+var magicE = null;
+
+randT = (hmax, hmin) => Math.floor(Math.random() * (hmax - hmin + 1)) + hmin;
+
 app.get('/greeting/:aname/', (req, res) => 
 {
     res.send('Wow! Hello There ' + `${req.params.aname}!`);
@@ -14,6 +23,37 @@ app.listen(port, () =>
 
 app.get('/tip/:total/:tipPercentage/', (req, res) => 
 {
-    res.send(`${req.params.tipPercentage}`);
+    var totalp = req.params.total / req.params.tipPercentage;
+    res.send(`${totalp}`);
 });
 
+app.get('/magic', (req, res) => {
+
+    
+    res.send('ask a question here...^');
+});
+
+app.get(`/magic/:callQ`, (req, res) =>
+{
+    magicE = req.params.callQ;
+    if (magicE != null) {
+        for (let i = 0; i < magicE.length; i++) {
+                magicE = magicE.replace(' ','%20');
+            }
+            magicE = req.params.callQ;
+        res.redirect(`/magic/callQ/${magicE}`);
+    }
+    else
+    {
+        res.send('ask again.');
+    }
+});
+
+app.get(`/magic/callQ/:response`, (req, res) =>{
+    if (magicE != null)
+    {
+        res.send(`<h1> ${magicE} : ${magic8[randT(hmin, hmax)]}</h1>`);
+        magicE = '';
+        res.send('ask another!...^');
+    }
+});
